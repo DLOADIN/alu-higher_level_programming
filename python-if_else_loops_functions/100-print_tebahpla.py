d3134c0ab279c6e15a7ba7f8f4cd2fbc3b1e9fe4
DLOADIN <m.david@alustudent.com>
2024-06-24 13:20:00 -0700

    file

--- python-if_else_loops_functions/100-print_tebahpla.py
+++ python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-for letter in range(122, 96, -1):
-    if letter % 2 != 0:
-        letter -= 32
-    print("{}".format(chr(letter)), end="")
+num = 122
+while num > 96:
+    print("{}".format(chr(num) if num % 2 == 0 else chr(num - 32)), end="")
+    num -= 1
