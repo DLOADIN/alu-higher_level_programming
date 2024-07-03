734a3749c082de0531d83b26b033e1506d5c42cf
DLOADIN <m.david@alustudent.com>
2024-07-03 02:44:29 -0700

    new

---
+++ python-import_modules/102-magic_calculation.py
@@ -0,0 +1,9 @@
+def magic_calculation(a, b):
+    from magic_calculation import add,sub
+    if a < b:
+        c = add(a,b)
+        for i in range(4,6):
+            c = add(i,c)
+        return c
+    else:
+        return sub(a,b)
